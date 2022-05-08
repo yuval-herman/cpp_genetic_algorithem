706d6d215ced137fb0267bcd2918fb4c7794d7b6
yuval <nhajeho1@gmail.com>
2022-05-08 12:28:54 +0300

    convert Genome to interface
    made Genome Virtual so that it will be possible to make any genome work in the code using the badic rules of the algorithem

--- Genome.h
+++ Genome.h
@@ -2,94 +2,17 @@
 #include "helpers.h"
 
 using namespace std;
+template <class T>
 
 class Genome
 {
 private:
-    string str = "";
     float fitness = 0.f;
 
-    char getRandomChar()
-    {
-        return (char)getRandomNum(32, 126);
-    }
-
 public:
-    Genome()
-    {
-        generateRandomStr();
-    }
-    Genome(string str)
-    {
-        this->str = str;
-    }
-
-    string getStr() { return str; }
     float getFitness() { return fitness; }
+    virtual float calculateFitness(string goal) = 0;
 
-    void generateRandomStr()
-    {
-        str.clear();
-        int len = getRandomNum(2, 1000);
-        for (int i = 0; i < len; i++)
-        {
-            str += getRandomChar();
-        }
-    }
-
-    Genome crossover(Genome mate)
-    {
-        string mateStr = mate.getStr();
-        bool useOurStr = getRandomNum(0, 1);
-        string newStr = (useOurStr) ? str : mateStr;
-        for (int i = 0; i < newStr.length(); i++)
-        {
-            if ((useOurStr) ? mateStr.length() : str.length() < i) // might cause problems, off-by-one-error TODO FIXME
-            {
-                newStr[i] = (!useOurStr) ? mateStr[i] : str[i];
-            }
-            else
-            {
-                newStr[i] = (getRandomNum(0, 1)) ? mateStr[i] : str[i];
-            }
-        }
-        return Genome(newStr);
-    }
-
-    Genome mutation()
-    {
-        for (int i = 0; i < str.length(); i++)
-        {
-            str[i] = (getRandomNum(0, 100) == 0) ? getRandomChar() : str[i];
-        }
-        if ((getRandomNum(0, 1000) == 0))
-            str.pop_back();
-        if ((getRandomNum(0, 1000) == 0))
-            str.push_back(getRandomChar());
-        return *this;
-    }
-
-    float calculateFitness(string goal)
-    {
-        if (goal.length() > str.length())
-        {
-            fitness = (float)str.length() / goal.length();
-        }
-        else
-        {
-            fitness = (float)goal.length() / str.length();
-        }
-        fitness /= 2;
-        float correctCharValue = 1.0f / goal.length() / 2;
-
-        for (int i = 0; i < min(goal.length(), str.length()); i++)
-        {
-            if (str[i] == goal[i])
-            {
-                fitness += correctCharValue;
-            }
-        }
-
-        return fitness;
-    }
+    virtual T crossover(T) = 0;
+    virtual T mutation() = 0;
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 
-#include "Genome.h"
+#include "StringGenome.h"
 
 #define POP_SIZE 300
 #define GENERATIONS 2000
@@ -16,14 +16,14 @@ int main(int argc, char *argv[])
         return 1;
     }
     string goal = argv[1];
-    Genome population[POP_SIZE];
-    Genome newPop[POP_SIZE];
-    Genome winner(goal);
+    StringGenome population[POP_SIZE];
+    StringGenome newPop[POP_SIZE];
+    StringGenome winner(goal);
     winner.calculateFitness(goal);
 
     for (int generation = 0; generation < GENERATIONS; generation++)
     {
-        sort(begin(population), end(population), [=](Genome a, Genome b) -> bool
+        sort(begin(population), end(population), [=](StringGenome a, StringGenome b) -> bool
              { return a.calculateFitness(goal) > b.calculateFitness(goal); });
 
         for (int i = 0; i < POP_SIZE; i++)
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
             {
                 second = getRandomNum(0, POP_SIZE / 5);
             }
-            Genome testGenome = population[first].crossover(population[second]).mutation();
+            StringGenome testGenome = population[first].crossover(population[second]).mutation();
             for (int j = 0; j < MAX_IMPROVE_TRIES; j++)
             {
                 if (testGenome.calculateFitness(goal) <= population[first].getFitness())
