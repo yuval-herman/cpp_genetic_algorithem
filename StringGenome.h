66444a0abba48a7e835c5a2343f83e79ce7c47fd
yuval <nhajeho1@gmail.com>
2022-05-08 12:31:42 +0300

    added StringGenome implementing Genome for strings

---
+++ StringGenome.h
@@ -0,0 +1,92 @@
+#include "Genome.h"
+
+class StringGenome : public Genome<StringGenome>
+{
+private:
+    string str = "";
+    float fitness = 0.f;
+
+    char getRandomChar()
+    {
+        return (char)getRandomNum(32, 126);
+    }
+
+public:
+    StringGenome()
+    {
+        generateRandomStr();
+    }
+    StringGenome(string str)
+    {
+        this->str = str;
+    }
+
+    string getStr() { return str; }
+    float getFitness() { return fitness; }
+
+    void generateRandomStr()
+    {
+        str.clear();
+        int len = getRandomNum(2, 1000);
+        for (int i = 0; i < len; i++)
+        {
+            str += getRandomChar();
+        }
+    }
+
+    StringGenome crossover(StringGenome mate)
+    {
+        string mateStr = mate.getStr();
+        bool useOurStr = getRandomNum(0, 1);
+        string newStr = (useOurStr) ? str : mateStr;
+        for (int i = 0; i < newStr.length(); i++)
+        {
+            if ((useOurStr) ? mateStr.length() : str.length() < i) // might cause problems, off-by-one-error TODO FIXME
+            {
+                newStr[i] = (!useOurStr) ? mateStr[i] : str[i];
+            }
+            else
+            {
+                newStr[i] = (getRandomNum(0, 1)) ? mateStr[i] : str[i];
+            }
+        }
+        return StringGenome(newStr);
+    }
+
+    StringGenome mutation()
+    {
+        for (int i = 0; i < str.length(); i++)
+        {
+            str[i] = (getRandomNum(0, 100) == 0) ? getRandomChar() : str[i];
+        }
+        if ((getRandomNum(0, 1000) == 0))
+            str.pop_back();
+        if ((getRandomNum(0, 1000) == 0))
+            str.push_back(getRandomChar());
+        return *this;
+    }
+
+    float calculateFitness(string goal)
+    {
+        if (goal.length() > str.length())
+        {
+            fitness = (float)str.length() / goal.length();
+        }
+        else
+        {
+            fitness = (float)goal.length() / str.length();
+        }
+        fitness /= 2;
+        float correctCharValue = 1.0f / goal.length() / 2;
+
+        for (int i = 0; i < min(goal.length(), str.length()); i++)
+        {
+            if (str[i] == goal[i])
+            {
+                fitness += correctCharValue;
+            }
+        }
+
+        return fitness;
+    }
+};
